038d55fc627ef7326afc6051a13a38fcc3ba87c6
Andrew Plummer <plummer.andrew@gmail.com>
2024-02-23 06:16:14 +0900

    allow relative links in externallink

--- web/src/components/ExternalLink.js
+++ web/src/components/ExternalLink.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 export default class ExternalLink extends React.Component {
+  getHref() {
+    const { to, href } = this.props;
+    if (href) {
+      return href;
+    } else if (to) {
+      return to;
+    }
+  }
+
   render() {
     const { href, children, icon, ...rest } = this.props;
     return (
       <a
-        href={href}
+        href={this.getHref()}
         target="_blank"
         rel="external noopener noreferrer"
         {...rest}>
